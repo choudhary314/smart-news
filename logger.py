c622298b1892d05b136b32371773e42d5499d57e
tarun <tarunonmail91@gmail.com>
2022-01-08 15:32:17 -0500

    instead of 1 day moved it to midnight for timeed rotating handler

--- logger.py
+++ logger.py
@@ -1,10 +1,10 @@
+from io import TextIOBase
 import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
 
 
 def setup_custom_logger(name):
-    # cwd = str(os.getcwd())
     script_dir = os.path.dirname(__file__)
     formatter_file = logging.Formatter(
         "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
@@ -14,9 +14,11 @@ def setup_custom_logger(name):
     except FileExistsError:
         pass
     path = os.path.join(script_dir, "logs", "trace.log")
-    handler = TimedRotatingFileHandler(path, when="d", interval=1, backupCount=10)
+    handler = TimedRotatingFileHandler(
+        path, when="midnight", interval=1, backupCount=10
+    )
     handler.setFormatter(formatter_file)
     logger = logging.getLogger(name)
-    logger.setLevel(level=logging.INFO)
+    logger.setLevel(logging.INFO)
     logger.addHandler(handler)
     return logger
